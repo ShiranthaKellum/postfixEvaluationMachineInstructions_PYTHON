a4dcea5cb59df780363e4bad54feb8bb4d45208f
Shirantha Jayasooriya <73046831+ShiranthaKellum@users.noreply.github.com>
2021-09-07 10:04:41 +0530

    Update postfixEavaluationMachineInstructionsGenerator.py

--- postfixEavaluationMachineInstructionsGenerator.py
+++ postfixEavaluationMachineInstructionsGenerator.py
@@ -1,102 +1,102 @@
 
-from queue import LifoQueue         # import queue module
+from queue import LifoQueue                         # import queue module
 
-stack = LifoQueue (maxsize=50)      # declare the stack
+stack = LifoQueue (maxsize=50)                      # declare the stack
 
-tempCount = '1'       # count number of the temp variables
+tempCount = '1'                                     # count number of the temp variables
 
 def addition ():
-    x = stack.get ()        # return the 1st popped element
-    y = stack.get ()        # return the 2nd popped element
+    x = stack.get ()                                # return the 1st popped element
+    y = stack.get ()                                # return the 2nd popped element
 
     if 'a' <= y <= 'z' or 'A' <= y <= 'Z':          # check for a user input variable
         print ("    LD       ", y)
 
     else:
-        print ("    LD      TEMP ", y)          # check for temp variable
+        print ("    LD      TEMP ", y)              # check for temp variable
 
-    if 'a' <= x <= 'z' or 'A' <= x <= 'Z':      # check for a user input variable
+    if 'a' <= x <= 'z' or 'A' <= x <= 'Z':          # check for a user input variable
         print("    AD       ", x)
 
     else:
-        print("    AD      TEMP ",  x)          # check for temp variable
+        print("    AD      TEMP ",  x)              # check for temp variable
     
-    global tempCount                            # specify as a global variable
+    global tempCount                                # specify as a global variable
     
     print ("    ST      TEMP ",  tempCount)
-    stack.put(tempCount)                       # push temp variable
+    stack.put(tempCount)                            # push temp variable
     tempCount = chr(ord(tempCount)+1)
 
 def substraction ():
-    x = stack.get()        # return the 1st popped element
-    y = stack.get()        # return the 2nd popped element
+    x = stack.get()                                 # return the 1st popped element
+    y = stack.get()                                 # return the 2nd popped element
 
     if 'a' <= y <= 'z' or 'A' <= y <= 'Z':          # check for a user input variable
         print("    LD      ", y)
 
     else:
-        print("    LD      TEMP ", y)          # check for temp variable
+        print("    LD      TEMP ", y)               # check for temp variable
 
-    if 'a' <= x <= 'z' or 'A' <= x <= 'Z':      # check for a user input variable
+    if 'a' <= x <= 'z' or 'A' <= x <= 'Z':          # check for a user input variable
         print("    SB       ", x)
 
     else:
-        print("    SB      TEMP ", x)          # check for temp variable
+        print("    SB      TEMP ", x)               # check for temp variable
 
     global tempCount
     print("    ST      TEMP ",  tempCount)
-    stack.put(tempCount)                       # push temp variable
+    stack.put(tempCount)                            # push temp variable
     tempCount = chr(ord(tempCount)+1)
 
 def multipication ():
-    x = stack.get()        # return the 1st popped element
-    y = stack.get()        # return the 2nd popped element
+    x = stack.get()                                 # return the 1st popped element
+    y = stack.get()                                 # return the 2nd popped element
 
     if 'a' <= y <= 'z' or 'A' <= y <= 'Z':          # check for a user input variable
         print("    LD      ", y)
 
     else:
-        print("    LD      TEMP ", y)          # check for temp variable
+        print("    LD      TEMP ", y)               # check for temp variable
 
-    if 'a' <= x <= 'z' or 'A' <= x <= 'Z':      # check for a user input variable
+    if 'a' <= x <= 'z' or 'A' <= x <= 'Z':          # check for a user input variable
         print("    ML       ", x)
 
     else:
-        print("    ML      TEMP ", x)          # check for temp variable
+        print("    ML      TEMP ", x)               # check for temp variable
 
     global tempCount
     print("    ST      TEMP ", tempCount)
-    stack.put(tempCount)                       # push temp variable
+    stack.put(tempCount)                            # push temp variable
     tempCount = chr(ord(tempCount)+1)
     
 def division ():
-    x = stack.get()        # return the 1st popped element
-    y = stack.get()        # return the 2nd popped element
+    x = stack.get()                                 # return the 1st popped element
+    y = stack.get()                                 # return the 2nd popped element
 
     if 'a' <= y <= 'z' or 'A' <= y <= 'Z':          # check for a user input variable
         print("    LD      ", y)
 
     else:
-        print("    LD      TEMP ", y)          # check for temp variable
+        print("    LD      TEMP ", y)               # check for temp variable
 
-    if 'a' <= x <= 'z' or 'A' <= x <= 'Z':      # check for a user input variable
+    if 'a' <= x <= 'z' or 'A' <= x <= 'Z':          # check for a user input variable
         print("    DV       ", x)
 
     else:
-        print("    DV      TEMP ", x)          # check for temp variable
+        print("    DV      TEMP ", x)               # check for temp variable
 
     global tempCount
     print("    ST      TEMP ",  tempCount)
-    stack.put(tempCount)                       # push temp variable
+    stack.put(tempCount)                            # push temp variable
     tempCount = chr(ord(tempCount)+1)
     
-expression = 'ABC*+DE-/, A+B/*C*+, AB+CD-*E+, ABCD+-*, AB+C*D/'
+expression = input ()                               #'ABC*+DE-/, A+B/*C*+, AB+CD-*E+, ABCD+-*, AB+C*D/'
 
 for i in expression:
-    if 'a' <= i <='z' or 'A' <= i <= 'Z':       # if it is an alphabet, push
-        stack.put (i)                           # push method
+    if 'a' <= i <='z' or 'A' <= i <= 'Z':           # if it is an alphabet, push
+        stack.put (i)                               # push method
 
-    elif i == '+':                              # else (operators), function are called
+    elif i == '+':                                  # else (operators), functions are called
         addition ()
 
     elif i == '-':
